977b441d0085f3c961c492802d4410575cbde85b
bijay2045 <bijay2045@gmail.com>
2022-10-28 11:47:52 +0545

    Footer Pattern Created

---
+++ footer.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Title: Footer
+ * Slug: footer
+ */
+?>
+<!-- wp:group {"layout":{"contentSize":"1280px","type":"constrained"},"align":"full","style":{"spacing":{"padding":{"top":"100px","bottom":"70px","right":"30px","left":"30px"}}},"backgroundColor":"black"} -->
+<div class="wp-block-group alignfull has-black-background-color has-background" style="padding-top:100px;padding-right:30px;padding-bottom:70px;padding-left:30px">
+    <!-- wp:columns {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","bottom":"0px"},"blockGap":{"top":"30px","left":"30px"}}}} -->
+    <div class="wp-block-columns" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
+        <!-- wp:column -->
+        <div class="wp-block-column">
+            <!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"uppercase"}}} -->
+            <h2 style="font-style:normal;font-weight:700;text-transform:uppercase">lorem</h2>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
+            <p style="font-size:16px">One of the main benefits of using Lorem Ipsum is that it can be easily generated, and it takes the pressure off designers to create meaningful text. Instead, they can focus on crafting the best website data.</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:group {"layout":{"type":"constrained","justifyContent":"left","contentSize":"180px"},"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px","margin":{"top":"0px","bottom":"0px"}}}} -->
+            <div class="wp-block-group" style="margin-top:0px;margin-bottom:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
+                <!-- wp:social-links {"iconColor":"white","iconColorValue":"#ffffff","openInNewTab":true,"align":"left","style":{"spacing":{"blockGap":{"top":"20px","left":"20px"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"left"}} -->
+                <ul class="wp-block-social-links alignleft has-icon-color is-style-logos-only">
+                    <!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+                    <!-- wp:social-link {"url":"#","service":"twitter"} /-->
+
+                    <!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+                    <!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+                </ul>
+                <!-- /wp:social-links -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"30px"}}}} -->
+        <div class="wp-block-column" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:30px">
+            <!-- wp:heading {"level":4,"style":{"typography":{"textTransform":"capitalize","fontStyle":"normal","fontWeight":"700","fontSize":"30px"}}} -->
+            <h4 style="font-size:30px;font-style:normal;font-weight:700;text-transform:capitalize">Contact Us</h4>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","lineHeight":"1.2"}}} -->
+            <p style="font-size:16px;line-height:1.2">123 BD Lorem, Ipsum<br><br>+123-456-7890</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","lineHeight":"1"}}} -->
+            <p style="font-size:16px;line-height:1">sample@gmail.com</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","lineHeight":"1"}}} -->
+            <p style="font-size:16px;line-height:1">Opening Hours: 10:00 - 18:00</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column">
+            <!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"30px","fontStyle":"normal","fontWeight":"700","textTransform":"capitalize"}}} -->
+            <h4 style="font-size:30px;font-style:normal;font-weight:700;text-transform:capitalize">Newsletter</h4>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
+            <p style="font-size:16px">Lorem ipsum dolor sit amet, consectetur ut labore et dolore magna aliqua ipsum dolor sit</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:search {"label":"","placeholder":"Enter your email...","buttonText":"Subscribe","buttonPosition":"button-inside","style":{"border":{"width":"2px","color":"#4000e7"},"color":{"background":"#4000e7"}},"textColor":"white"} /-->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
