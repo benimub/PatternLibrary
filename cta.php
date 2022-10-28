ac627f9b4b62ee8c3c4f517585a5bc0962860ac0
bijay2045 <bijay2045@gmail.com>
2022-10-28 11:39:39 +0545

    CTA Pattern Created

---
+++ cta.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Title: CTA
+ * Slug: cta
+ */
+?>
+<!-- wp:columns {"align":"wide","style":{"border":{"radius":"0px"}},"borderColor":"vivid-purple"} -->
+<div class="wp-block-columns alignwide has-border-color has-vivid-purple-border-color" style="border-radius:0px">
+    <!-- wp:column {"style":{"spacing":{"padding":{"top":"90px","right":"90px","bottom":"90px","left":"90px"}}},"backgroundColor":"vivid-purple"} -->
+    <div class="wp-block-column has-vivid-purple-background-color has-background" style="padding-top:90px;padding-right:90px;padding-bottom:90px;padding-left:90px">
+        <!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"white","fontSize":"large"} -->
+        <h2 class="has-text-align-center has-white-color has-text-color has-large-font-size" style="font-style:normal;font-weight:700">Fasten your website creation process with CTA Block</h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:buttons -->
+        <div class="wp-block-buttons">
+            <!-- wp:button {"backgroundColor":"white","align":"center","style":{"color":{"text":"#8c30f5"},"spacing":{"padding":{"top":"8px","right":"20px","bottom":"8px","left":"20px"}},"border":{"radius":"7px"}},"className":"is-style-fill","fontSize":"small"} -->
+            <div class="wp-block-button aligncenter has-custom-font-size is-style-fill has-small-font-size"><a class="wp-block-button__link has-white-background-color has-text-color has-background" style="border-radius:7px;color:#8c30f5;padding-top:8px;padding-right:20px;padding-bottom:8px;padding-left:20px"><strong>Download Now</strong></a></div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:buttons -->
+    </div>
+    <!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
