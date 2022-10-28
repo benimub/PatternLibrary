e2914f87eeb24140b5534b842780b39116cf5346
bijay2045 <bijay2045@gmail.com>
2022-10-28 11:55:26 +0545

    4040 Error Patter Created

---
+++ 404-error.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Title: 404 Error
+ * Slug: not-found-error
+ */
+?>
+<!-- wp:columns {"style":{"border":{"radius":"0px"}}} -->
+<div class="wp-block-columns" style="border-radius:0px">
+    <!-- wp:column {"verticalAlignment":"center","width":"","style":{"border":{"color":"#0054bd","width":"2px"},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}},"color":{"background":"#f1f7fb"}},"layout":{"type":"constrained","wideSize":"100px","contentSize":"96%"}} -->
+    <div class="wp-block-column is-vertically-aligned-center has-border-color has-background" style="border-color:#0054bd;border-width:2px;background-color:#f1f7fb;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
+        <!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700","textTransform":"capitalize","fontSize":"160px"},"color":{"text":"#0054bd"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+        <h2 class="has-text-align-center has-text-color" style="color:#0054bd;margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:160px;font-style:normal;font-weight:700;text-transform:capitalize"><strong>404</strong></h2>
+        <!-- /wp:heading -->
+
+        <!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"x-large"} -->
+        <h3 class="has-text-align-center has-x-large-font-size" style="font-style:normal;font-weight:500">Page Not Found</h3>
+        <!-- /wp:heading -->
+
+        <!-- wp:spacer {"height":"15px"} -->
+        <div style="height:15px" aria-hidden="true" class="wp-block-spacer"></div>
+        <!-- /wp:spacer -->
+
+        <!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"400"}}} -->
+        <h4 class="has-text-align-center" style="font-size:18px;font-style:normal;font-weight:400">We're sorry, the page you requested could not be found.<br>Please go back to the homepage.</h4>
+        <!-- /wp:heading -->
+
+        <!-- wp:spacer {"height":"20px"} -->
+        <div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+        <!-- /wp:spacer -->
+
+        <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+        <div class="wp-block-buttons">
+            <!-- wp:button {"textColor":"white","align":"right","style":{"spacing":{"padding":{"top":"10px","right":"10px","bottom":"10px","left":"10px"}},"border":{"radius":"30px"},"color":{"background":"#0054bd"}},"fontSize":"medium"} -->
+            <div class="wp-block-button alignright has-custom-font-size has-medium-font-size"><a class="wp-block-button__link has-white-color has-text-color has-background wp-element-button" href="#" style="border-radius:30px;background-color:#0054bd;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px">Back to Homepage</a></div>
+            <!-- /wp:button -->
+        </div>
+        <!-- /wp:buttons -->
+
+        <!-- wp:spacer {"height":"20px"} -->
+        <div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+        <!-- /wp:spacer -->
+    </div>
+    <!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
