49a93f4acde1f04c1a61921761872e5cabf65d4c
bijay2045 <bijay2045@gmail.com>
2022-10-28 11:28:35 +0545

    New Pattern for testimonials created

---
+++ testimonials.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Title: Client Testimonials
+ * Slug: client-testimonials
+ */
+?>
+<!-- wp:group {"style":{"color":{"background":"#eff7fb"},"spacing":{"padding":{"top":"40px","right":"20px","bottom":"40px","left":"20px"}}}} -->
+<div class="wp-block-group has-background" style="background-color:#eff7fb;padding-top:40px;padding-right:20px;padding-bottom:40px;padding-left:20px">
+    <!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"color":{"text":"#472f8c"}}} -->
+    <h2 class="has-text-align-center has-text-color" style="color:#472f8c;font-style:normal;font-weight:700">Testimonials</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"28px"},"color":{"text":"#472f8c"}}} -->
+    <h3 class="has-text-align-center has-text-color" style="color:#472f8c;font-size:28px">What Our Clients Think About Us</h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:columns -->
+    <div class="wp-block-columns">
+        <!-- wp:column {"style":{"color":{"background":"#ffffff00"},"spacing":{"padding":{"top":"20px","right":"20px","bottom":"20px","left":"20px"}}}} -->
+        <div class="wp-block-column has-background" style="background-color:#ffffff00;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px">
+            <!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"25px","bottom":"30px","left":"25px"}},"border":{"width":"1px","style":"solid","radius":"10px","color":"#76b0dd"},"color":{"background":"#fffefe","text":"#212121"}}} -->
+            <div class="wp-block-group has-border-color has-text-color has-background" style="background-color:#fffefe;border-color:#76b0dd;border-radius:10px;border-style:solid;border-width:1px;color:#212121;padding-top:30px;padding-right:25px;padding-bottom:30px;padding-left:25px">
+                <!-- wp:paragraph {"align":"center","style":{"color":{"text":"#1c0d5a"}}} -->
+                <p class="has-text-align-center has-text-color" style="color:#1c0d5a"><strong>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</strong></p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"24px"}},"textColor":"vivid-cyan-blue"} -->
+                <h3 class="has-text-align-center has-vivid-cyan-blue-color has-text-color" style="font-size:24px;font-style:normal;font-weight:700">John Doe</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"16px"},"spacing":{"margin":{"top":"10px","right":"0px","bottom":"15px","left":"0px"}}},"textColor":"vivid-cyan-blue"} -->
+                <h3 class="has-text-align-center has-vivid-cyan-blue-color has-text-color" style="font-size:16px;font-style:normal;font-weight:500;margin-top:10px;margin-right:0px;margin-bottom:15px;margin-left:0px">Chief Director</h3>
+                <!-- /wp:heading -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"style":{"color":{"background":"#ffffff00"},"spacing":{"padding":{"top":"20px","right":"20px","bottom":"20px","left":"20px"}}}} -->
+        <div class="wp-block-column has-background" style="background-color:#ffffff00;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px">
+            <!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"25px","bottom":"30px","left":"25px"}},"border":{"width":"1px","style":"solid","radius":"10px","color":"#76b0dd"}},"backgroundColor":"white","textColor":"vivid-cyan-blue"} -->
+            <div class="wp-block-group has-border-color has-vivid-cyan-blue-color has-white-background-color has-text-color has-background" style="border-color:#76b0dd;border-radius:10px;border-style:solid;border-width:1px;padding-top:30px;padding-right:25px;padding-bottom:30px;padding-left:25px">
+                <!-- wp:paragraph {"align":"center","style":{"color":{"text":"#1c0d5a"}}} -->
+                <p class="has-text-align-center has-text-color" style="color:#1c0d5a"><strong>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</strong></p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"24px"}},"textColor":"vivid-cyan-blue"} -->
+                <h3 class="has-text-align-center has-vivid-cyan-blue-color has-text-color" style="font-size:24px;font-style:normal;font-weight:700">John Doe</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"16px"},"spacing":{"margin":{"top":"10px","right":"0px","bottom":"15px","left":"0px"}}},"textColor":"vivid-cyan-blue"} -->
+                <h3 class="has-text-align-center has-vivid-cyan-blue-color has-text-color" style="font-size:16px;font-style:normal;font-weight:500;margin-top:10px;margin-right:0px;margin-bottom:15px;margin-left:0px">Chief Director</h3>
+                <!-- /wp:heading -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons">
+        <!-- wp:button {"textColor":"white","style":{"color":{"background":"#0e94ff"},"spacing":{"padding":{"top":"10px","right":"30px","bottom":"10px","left":"30px"}},"border":{"radius":"5px"}},"className":"is-style-fill"} -->
+        <div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-white-color has-text-color has-background" style="border-radius:5px;background-color:#0e94ff;padding-top:10px;padding-right:30px;padding-bottom:10px;padding-left:30px">View More</a></div>
+        <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+</div>
+<!-- /wp:group -->
