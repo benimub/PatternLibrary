1b0a7a314d0aaf3205eae2c39d6b8cb10b62a475
Trinisha <87406714+trinisha05@users.noreply.github.com>
2022-10-31 13:21:29 +0545

    Create counter pattern
    Counter Up/Countdown timer design

---
+++ counter-pattern.php
@@ -0,0 +1,39 @@
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"60px","bottom":"20px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:60px;margin-bottom:20px"><!-- wp:heading {"textAlign":"center","style":{"color":{"text":"#251c74"},"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"0","left":"var:preset|spacing|50"}},"typography":{"fontSize":"40px","lineHeight":"1.2","letterSpacing":"0px","fontStyle":"normal","fontWeight":"600"}}} -->
+<h2 class="has-text-align-center has-text-color" style="color:#251c74;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50);font-size:40px;font-style:normal;font-weight:600;letter-spacing:0px;line-height:1.2">Countdown Start</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"color":{"text":"#696e8d"},"spacing":{"padding":{"right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"10px"}},"typography":{"fontSize":"14px"}}} -->
+<p class="has-text-align-center has-text-color" style="color:#696e8d;margin-top:10px;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);font-size:14px">The countdown has begun for the launch of the space shuttle</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0px"}},"color":{"background":"#7c81a1"}},"className":"is-style-default"} -->
+<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-default" style="margin-top:0px;background-color:#7c81a1;color:#7c81a1" />
+<!-- /wp:separator -->
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"50px","bottom":"80px"}}}} -->
+<div class="wp-block-columns alignwide" style="margin-top:50px;margin-bottom:80px"><!-- wp:column {"width":"100%","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:heading {"textAlign":"center","style":{"color":{"text":"#4941e9"},"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"100px","lineHeight":"1"}}} -->
+<h2 class="has-text-align-center has-text-color" style="color:#4941e9;font-size:100px;font-style:normal;font-weight:500;line-height:1">9</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:heading {"textAlign":"center","style":{"color":{"text":"#f92b8b"},"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"100px","lineHeight":"1"}}} -->
+<h2 class="has-text-align-center has-text-color" style="color:#f92b8b;font-size:100px;font-style:normal;font-weight:500;line-height:1">10</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:heading {"textAlign":"center","style":{"color":{"text":"#06caff"},"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"100px","lineHeight":"1"}}} -->
+<h2 class="has-text-align-center has-text-color" style="color:#06caff;font-size:100px;font-style:normal;font-weight:500;line-height:1">53</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:100%"><!-- wp:heading {"textAlign":"center","style":{"color":{"text":"#34dd87"},"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"100px","lineHeight":"1"}}} -->
+<h2 class="has-text-align-center has-text-color" style="color:#34dd87;font-size:100px;font-style:normal;font-weight:500;line-height:1">43</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
