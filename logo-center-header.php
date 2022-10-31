5814cd53b802c035c637d3f2acb84ddcb455d7c3
Trinisha <87406714+trinisha05@users.noreply.github.com>
2022-10-31 13:18:30 +0545

    create logo-center-header file

---
+++ logo-center-header.php
@@ -0,0 +1,49 @@
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"},"style":{"color":{"background":"#ebebeb"},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-group has-background" style="background-color:#ebebeb;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:columns {"isStackedOnMobile":false,"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-columns is-not-stacked-on-mobile" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:verse -->
+<pre class="wp-block-verse">Hello, from the other side!!</pre>
+<!-- /wp:verse --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:social-links {"iconColor":"black","iconColorValue":"#000000","align":"right","className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"}} -->
+<ul class="wp-block-social-links alignright has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"wordpress"} /-->
+
+<!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"skype"} /-->
+
+<!-- wp:social-link {"url":"#","service":"tumblr"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontSize":"60px","letterSpacing":"2px"}}} -->
+<h1 class="has-text-align-center" style="font-size:60px;letter-spacing:2px"><strong>LOGO</strong></h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"},"style":{"border":{"top":{"color":"var:preset|color|cyan-bluish-gray","width":"2px"},"bottom":{"color":"var:preset|color|cyan-bluish-gray","width":"2px"}},"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"fontSize":"medium"} -->
+<div class="wp-block-group has-medium-font-size" style="border-top-color:var(--wp--preset--color--cyan-bluish-gray);border-top-width:2px;border-bottom-color:var(--wp--preset--color--cyan-bluish-gray);border-bottom-width:2px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>About Us</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Services</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Contact Us</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Resources</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
