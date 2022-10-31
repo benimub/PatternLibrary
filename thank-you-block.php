b1d7acd3be7e0655c9aaeef59ec86497592be0d7
Trinisha <87406714+trinisha05@users.noreply.github.com>
2022-10-31 13:28:01 +0545

    Thank you block
    Thank you message block

---
+++ thank-you-block.php
@@ -0,0 +1,23 @@
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"}},"border":{"radius":{"topLeft":"80px","topRight":"80px","bottomLeft":"80px","bottomRight":"0px"},"color":"#4b1bce","width":"5px"}},"backgroundColor":"black","layout":{"type":"constrained","contentSize":"650px"}} -->
+<div class="wp-block-group alignwide has-border-color has-black-background-color has-background" style="border-color:#4b1bce;border-width:5px;border-top-left-radius:80px;border-top-right-radius:80px;border-bottom-left-radius:80px;border-bottom-right-radius:0px;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"44px","textTransform":"none"},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}},"textColor":"white"} -->
+<h2 class="has-text-align-center has-white-color has-text-color" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;font-size:44px;font-style:normal;font-weight:700;text-transform:none"> 🎉  Congratulations on your purchase</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"800","fontSize":"22px","textTransform":"capitalize"},"spacing":{"padding":{"top":"20px","right":"0px","bottom":"15px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+<h3 class="has-text-align-center" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;padding-top:20px;padding-right:0px;padding-bottom:15px;padding-left:0px;font-size:22px;font-style:normal;font-weight:800;text-transform:capitalize">Your Mail Sent Successfully</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"45px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}},"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"16px"},"color":{"text":"#a6a6a6"}}} -->
+<p class="has-text-align-center has-text-color" style="color:#a6a6a6;margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;padding-top:0px;padding-right:0px;padding-bottom:45px;padding-left:0px;font-size:16px;font-style:normal;font-weight:700">Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"blockGap":"0px","margin":{"top":"0px","bottom":"0px"}}}} -->
+<div class="wp-block-buttons" style="margin-top:0px;margin-bottom:0px"><!-- wp:button {"textColor":"white","style":{"border":{"radius":{"topRight":"30px","bottomRight":"0px","topLeft":"30px","bottomLeft":"30px"}},"spacing":{"padding":{"top":"12px","right":"30px","bottom":"12px","left":"30px"}},"color":{"background":"#4b1bce"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-white-color has-text-color has-background wp-element-button" style="border-top-left-radius:30px;border-top-right-radius:30px;border-bottom-left-radius:30px;border-bottom-right-radius:0px;background-color:#4b1bce;padding-top:12px;padding-right:30px;padding-bottom:12px;padding-left:30px">Back to Home</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
